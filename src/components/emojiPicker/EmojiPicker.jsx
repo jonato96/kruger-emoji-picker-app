e02f2457141ae80b77a6ffef8ef2356a57c3d320
Jonathan <jonato96s@gmail.com>
2023-01-08 09:18:07 -0500

    Adding filter emoji list

--- src/components/emojiPicker/EmojiPicker.jsx
+++ src/components/emojiPicker/EmojiPicker.jsx
@@ -1,28 +1,47 @@
 import {forwardRef, useState} from 'react'
 import {data as emojiList} from "./data"
+import EmojiSearch from './EmojiSearch'
 
 export function EmojiPicker(props, inputRef){
     const [isOpen, setIsOpen] = useState(false)
-    const [emojis, setEmojis] = useState(emojiList)
+    const [emojis, setEmojis] = useState([...emojiList])
     function handleClickOpen(){
         setIsOpen(!isOpen)
     }
-    function EmojiPickerContainer(){
-        return(
-            <div>
-                <input />
-                <div>
-                    {emojiList.map(emoji => <div>{emoji.symbol}</div>)}
-                </div>
-            </div>
-        )
+    function handleSearch(e){
+        const q = e.target.value        
+        if(!!q){            
+            const search = emojiList.filter((emoji) => {
+                return (
+                    emoji.name.toLowerCase().includes(q) ||
+                    emoji.keywords.toLowerCase().includes(q))
+            })
+            console.log(search)
+            setEmojis(search)
+        }else{
+            setEmojis(emojiList)
+        }
     }
+    // function EmojiPickerContainer(){
+    //     return(
+    //         <div>
+    //             <EmojiSearch onSearch={handleSearch}/>
+    //             <div>
+    //                 {emojiList.map(emoji => <div key={emoji.symbol}>{emoji.symbol}</div>)}
+    //             </div>
+    //         </div>
+    //     )
+    // }
     return(
         <div>
             <button onClick={handleClickOpen}>⌨️</button>
-            {isOpen ? <EmojiPickerContainer/> : ""}
+            {isOpen ?
+            (<div>
+                <EmojiSearch onSearch={handleSearch}/>
+                <div>
+                    {emojis.map((emoji) => (<div key={emoji.symbol}>{emoji.symbol}</div>))}
+                </div>
+            </div>) : ("")}
         </div>
-    )
-}
-
+    )}
 export default forwardRef(EmojiPicker)
---
+++ src/components/emojiPicker/EmojiSearch.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const EmojiSearch = ({onSearch}) =>{
+    const [value, setValue] = useState("")
+    function handleChange(e){
+        setValue(e.target.value)
+        onSearch(e)
+    }
+    return(
+        <input type="text" onChange={handleChange}/>
+    )
+}
+export default EmojiSearch
